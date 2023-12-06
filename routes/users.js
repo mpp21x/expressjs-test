9b36ac442377b9bd33aa00ff87d44d2ef8325934
matt <matt@zuso.ai>
2023-12-06 23:21:49 +0800

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const students = [
+    {name: 'John', age: 20, score: 85},
+    {name: 'Mary', age: 22, score: 90},
+    {name: 'Jane', age: 21, score: 75},
+  ];
+
+  const theStudentsIsGreaterThen3 = students.length > 3;
+
+  res.render('user-index', {
+    title: 'Users',
+    students,
+    theStudentsIsGreaterThen3,
+    test: {
+      name: 'matt'
+    }
+  });
 });
 
 module.exports = router;
